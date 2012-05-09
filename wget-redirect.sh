8a4ab766c484afa85cb23dc4a54b8473d3b5acb7
Maverick JS <mavjs01@gmail.com>
2012-05-09 12:24:56 +0800

    a script to unshorten links without opening the browser

---
+++ wget-redirect.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -vx
+if [ "$#" -ne 2 ]; then
+    echo "Error: url not supplied."
+    echo "Usage: $0  \"redirect times\" \"url\""
+    exit 1
+fi
+`wget --max-redirect=$1 $2 | grep -v "Location"`
